df0377d8aa1d57ff6c4f44bc3c37b26ee2149b7f
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2018-01-30 23:18:27 +0900

    fix indent

--- popup/popup_menu.js
+++ popup/popup_menu.js
@@ -9,17 +9,17 @@ window.addEventListener( 'load', function(e){
 	let checkbox_element = document.getElementById('ld_is_enabled');
 
 	checkbox_element.addEventListener('change', function(e) {
-		console.debug('popup is_enable change');
+	console.debug('popup is_enable change');
 
-		// message to current tab content_script
-		browser.tabs.query({active: true, currentWindow: true}, tabs => {
-			browser.tabs.sendMessage(
-					tabs[0].id,
-					{ type: 'popup_change' }
-					).then(response => {
-						console.log(response.response);
-					}).catch(onError);
-					});
+	// message to current tab content_script
+	browser.tabs.query({active: true, currentWindow: true}, tabs => {
+		browser.tabs.sendMessage(
+				tabs[0].id,
+				{ type: 'popup_change' }
+				).then(response => {
+					console.log(response.response);
+				}).catch(onError);
+				});
 	});
 
 }, false);
